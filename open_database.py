055d433f18bd7471d6d5467be2dde99a4cc52f85
Andrew Schwartz <andrew@aschwartz.me>
2018-11-07 22:02:45 -0700

    add method to get a team's record in a given season

--- open_database.py
+++ open_database.py
@@ -18,6 +18,13 @@ def get_teams_list(connection):
 	print(rows)
 
 
+def get_team_record(connection, team, year):
+	cur = connection.cursor()
+	cur.execute("SELECT ( CAST(W AS FLOAT) / (W+L)), W, L from teams where teamID is '{0}' and yearID is '{1}'".format(team, year))
+	rows = cur.fetchall()
+	print(rows)
+
+
 def get_annual_salary(connection, team, year):
 	cur = connection.cursor()
 	cur.execute("SELECT sum(salary) from salaries where teamID is '{0}' and yearID is '{1}'".format(team, year))
@@ -32,7 +39,8 @@ def main():
 	# create a database connection
 	conn = create_connection(database)
 	# get_annual_salary(conn, 'WAS', 2005)
-	get_teams_list(conn)
+	# get_teams_list(conn)
+	get_team_record(conn, 'COL', 2008)
 
 if __name__ == '__main__':
 	main()
