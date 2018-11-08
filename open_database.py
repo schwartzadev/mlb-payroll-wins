9710af5a48f3ad25c5dc00c1384a69c787b8fce7
Andrew Schwartz <andrew@aschwartz.me>
2018-11-07 22:21:14 -0700

    add record information to salary query with join

--- open_database.py
+++ open_database.py
@@ -20,14 +20,19 @@ def get_teams_list(connection):
 
 def get_team_record(connection, team, year):
 	cur = connection.cursor()
-	cur.execute("SELECT ( CAST(W AS FLOAT) / (W+L)), W, L from teams where teamID is '{0}' and yearID is '{1}'".format(team, year))
+	cur.execute("SELECT ( CAST(W AS FLOAT) / (W+L) ), W, L from teams where teamID is '{0}' and yearID is '{1}'".format(team, year))
 	rows = cur.fetchall()
 	print(rows)
 
 
-def get_annual_salary(connection, team, year):
+def get_annual_salary_and_record(connection, team, year):
 	cur = connection.cursor()
-	cur.execute("SELECT sum(salary) from salaries where teamID is '{0}' and yearID is '{1}'".format(team, year))
+	cur.execute("""
+		SELECT sum(salaries.salary), salaries.teamID, salaries.yearID, ( CAST(teams.W AS FLOAT) / (teams.W+teams.L) ), teams.W, teams.L
+		FROM salaries
+		JOIN teams ON teams.teamID = salaries.teamID AND teams.yearID = salaries.yearID
+		WHERE salaries.teamID is '{0}' AND salaries.yearID is '{1}';
+		""".format(team, year))
 	rows = cur.fetchall()
 	print(rows)
 
@@ -38,9 +43,11 @@ def main():
 
 	# create a database connection
 	conn = create_connection(database)
-	# get_annual_salary(conn, 'WAS', 2005)
-	# get_teams_list(conn)
-	get_team_record(conn, 'COL', 2008)
+	team = 'COL'
+	year = 2008
+	get_annual_salary_and_record(conn, team, year)
+	# get_teams_list(conns)
+	get_team_record(conn, team, year)
 
 if __name__ == '__main__':
 	main()
